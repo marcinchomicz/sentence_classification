f3777514fa838bd52a51906f581e2e1fc1233c80
marcinchomicz <marcin.chomicz@mch.net.pl>
2022-11-28 19:31:42 +0100

    Optimization script for model with BiLSTM-CNN branch for sentence classified, and two dense branches for preceding and following sentences

--- sandbox/mail_line_classif_RNN_sequential.py
+++ sandbox/mail_line_classif_RNN_sequential.py
@@ -1,7 +1,8 @@
+
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = ""
-# os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = "true"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = "true"
 import pandas as pd
 import tensorflow as tf
 import tensorflow_addons as tfa
@@ -11,12 +12,14 @@ from sklearn.model_selection import train_test_split
 import itertools
 from tqdm import tqdm
 
+
+
 for m in [pd, tf, tfa, mlflow]:
     print(f"{m.__name__:15s}\t{m.__version__}")
 
 OUTPUTPATH = "/mnt/workdata/_WORK_/mail_zonning/mail_zoning/sandbox"
 
-DATAPATH = "/mnt/workdata/_WORK_/mail_zonning/repos/Quagga/Datasets/Enron/annotated_lines/"
+DATAPATH = "/mnt/workdata/_WORK_/mail_zonning/mail_zoning/dataset/enron_files_annotated/"
 
 def prepare_dataset(datapath:str):
 
@@ -58,60 +61,18 @@ def split_dataset(data: pd.DataFrame):
 
 df=prepare_dataset(DATAPATH)
 train_data, train_labels, test_data, test_labels = split_dataset(df)
-print(train_data.shape)
-print(train_labels.shape)
-print(test_data.shape)
-print(test_labels.shape)
-
 
 mp = {
-    'vocab_size': 10000,
+    'vocab_size': 7500,
     'hidden_size': 32,
     'lr_reduction_factor': 0.1,
     'rnn_variant': 'BiLSTM',
 }
 
 
-def define_layers(model_params: dict, vectorization_layer):
-    if model_params['rnn_variant'] == 'StackedBiLSTM':
-        rnn_layers = [
-            tf.keras.layers.Bidirectional(
-                layer=tf.keras.layers.LSTM(units=model_params['hidden_size'], return_sequences=True), name='BiLSTM_0'),
-            tf.keras.layers.Bidirectional(
-                layer=tf.keras.layers.LSTM(units=model_params['hidden_size']), name='BiLSTM_1')
-        ]
-    elif model_params['rnn_variant'] == 'BiGRU':
-        rnn_layers = [
-            tf.keras.layers.Bidirectional(
-                layer=tf.keras.layers.GRU(units=model_params['hidden_size'], return_sequences=False, name='BiGRU'))
-        ]
-    elif model_params['rnn_variant'] == 'StackedBiGRU':
-        rnn_layers = [
-            tf.keras.layers.Bidirectional(
-                layer=tf.keras.layers.GRU(units=model_params['hidden_size'], return_sequences=True, name='BiGRU_0')),
-            tf.keras.layers.Bidirectional(
-                layer=tf.keras.layers.GRU(units=model_params['hidden_size'], return_sequences=False, name='BiGRU_1'))
-        ]
-    else:
-        rnn_layers = [tf.keras.layers.Bidirectional(
-            layer=tf.keras.layers.LSTM(units=model_params['hidden_size']), name='BiLSTM')]
-
-    layers = [
-        vectorization_layer,
-        tf.keras.layers.Embedding(input_dim=len(vect_layer.get_vocabulary()),
-                                  input_length=model_params['output_sequence_length'],
-                                  output_dim=model_params['hidden_size'],
-                                  mask_zero=True,
-                                  name='Embedder')
-    ]
-    layers.extend(rnn_layers)
-    layers.append(tf.keras.layers.Dense(units=model_params['hidden_size'], activation='relu', name='Dense_1'))
-    layers.append(tf.keras.layers.Dense(units=label_count, use_bias=True, activation='softmax', name='Output'))
-
-    return layers
 
 mlflow.set_tracking_uri("file:///home/chomima5/mlruns")
-ENAME = 'SencenceClassifRNN_Sequential'
+ENAME = 'SencenceClassifCNN'
 
 try:
     eid = mlflow.get_experiment_by_name(ENAME).experiment_id
@@ -119,37 +80,113 @@ except:
     eid = mlflow.create_experiment(ENAME)
 
 
-vocab_size_scope = [2000] # 1000, 2500, 5000, 7500, 10000
+def define_model_BiLSTM(adapted_text_vectorization_layer, model_params: dict, label_count: int):
+    """ Define model using functional API"""
+    tf.keras.backend.clear_session()
+
+    # Input layer is required, datatype and shape must be provided as well
+    inputs = tf.keras.layers.Input(shape=(1,), dtype=tf.string)
+    x = adapted_text_vectorization_layer(inputs)
+
+    # define embedding layer
+    # get real vocab size, after adapting TextVectorization
+    x = tf.keras.layers.Embedding(input_dim=len(adapted_text_vectorization_layer.get_vocabulary()),
+                                           output_dim=model_params['hidden_size'],
+                                           mask_zero=True, name='Embedder')(x)
+    #define BiLSTM and Dense layers
+    x = tf.keras.layers.Bidirectional(
+        layer=tf.keras.layers.LSTM(units=mp['hidden_size']), name='BiLSTM')(x)
+    x = tf.keras.layers.Dense(units=mp['hidden_size'], activation='relu', name='Dense_1')(x)
+    output_layer = tf.keras.layers.Dense(units=label_count, use_bias=True, activation='softmax', name='Output')(x)
+
+    model = tf.keras.models.Model(inputs=inputs, outputs=output_layer)
+    return model
+
+def define_model_CNN(adapted_text_vectorization_layer, model_params: dict, label_count:int):
+    tf.keras.backend.clear_session()
+    inputs=tf.keras.layers.Input(shape=(1,), dtype=tf.string)
+    x=adapted_text_vectorization_layer(inputs)
+    x = tf.keras.layers.Embedding(input_dim=len(adapted_text_vectorization_layer.get_vocabulary()),
+                                  output_dim=model_params['hidden_size'],
+                                  mask_zero=True,
+                                  name='Embedder')(x)
+    x1= tf.keras.layers.Convolution1D(
+        filters=model_params['Conv1D_size'],
+        kernel_size=model_params['Conv1D_kernel_size'],
+        strides=1,
+        padding='valid',
+        activation='relu')(x)
+
+    x1 = tf.keras.layers.MaxPooling1D(pool_size=model_params['Pool1D_pool_size'])(x1)
+    x1 = tf.keras.layers.Flatten()(x1)
+
+    x= tf.keras.layers.Dense(model_params['hidden_size'])(x1)
+    output_layer=tf.keras.layers.Dense(units=label_count, use_bias=True, activation='softmax', name='Output')(x)
+    model = tf.keras.models.Model(inputs=inputs, outputs=output_layer)
+    return model
+
+def define_model_CNN_LSTM(adapted_text_vectorization_layer, model_params: dict, label_count:int):
+    tf.keras.backend.clear_session()
+    inputs=tf.keras.layers.Input(shape=(1,), dtype=tf.string)
+    x=adapted_text_vectorization_layer(inputs)
+    x = tf.keras.layers.Embedding(input_dim=len(adapted_text_vectorization_layer.get_vocabulary()),
+                                  output_dim=model_params['hidden_size'],
+                                  mask_zero=True,name='Embedder')(x)
+
+
+    x1= tf.keras.layers.Convolution1D(
+        filters=model_params['Conv1D_size'],
+        kernel_size=model_params['Conv1D_kernel_size'],
+        strides=1,
+        padding='valid',
+        activation='relu')(x)
+    x1 = tf.keras.layers.MaxPooling1D(pool_size=model_params['Pool1D_pool_size'])(x1)
+    x1 = tf.keras.layers.Flatten()(x1)
+    x1 = tf.keras.layers.Dense(model_params['hidden_size'])(x1)
+
+    x2 = tf.keras.layers.Bidirectional(
+        layer=tf.keras.layers.LSTM(units=mp['hidden_size'], return_sequences=True), name='BiLSTM')(x)
+    x2 = tf.keras.layers.Dense(units=mp['hidden_size'],
+                                    activation='relu',
+                                    name='Dense_1',
+                                    use_bias=True,
+                                    bias_initializer='glorot_uniform')(x2)
+
+    x=tf.keras.layers.Concatenate(axis=1)([x1, x2])
+
+
+    output_layer=tf.keras.layers.Dense(units=label_count, use_bias=True, activation='softmax', name='Output')(x)
+    model = tf.keras.models.Model(inputs=inputs, outputs=output_layer)
+    return model
+
+
+vocab_size_scope = [7500] # 1000, 2500, 5000, 7500, 10000
 hidden_size_scope = [64] # 16, 32, 64, 96, 128, 256
-rnn_variants = ['BiLSTM'] # 'BiLSTM', 'StackedBiLSTM', 'BiGRU', 'StackedBiGRU'
+rnn_variants = ['CNN'] # 'BiLSTM', 'StackedBiLSTM', 'BiGRU', 'StackedBiGRU'
 
 scope = list(itertools.product(vocab_size_scope, hidden_size_scope, rnn_variants))
-
 for vocab_size, hidden_size, variant in tqdm(scope, total=len(scope)):
     mp['vocab_size'] = vocab_size
     mp['hidden_size'] = hidden_size
     mp['rnn_variant'] = variant
     mp['split'] = 'document_based'
     mp['output_sequence_length'] = 300
+    mp['Conv1D_size'] = 64
+    mp['Conv1D_kernel_size']=3
+    mp['Pool1D_pool_size']=2
 
-    tf.keras.backend.clear_session()
-    vect_layer = tf.keras.layers.TextVectorization(max_tokens=mp['vocab_size'],
-                                                   output_mode='int',
-                                                   name='Vectorizer')
-    vect_layer.adapt(data=df['sentence'].values)
+
+    # create and adapt vectorization layer
+    vectorization_layer = tf.keras.layers.TextVectorization(max_tokens=mp['vocab_size'],
+                                                            output_sequence_length=300,
+                                                            pad_to_max_tokens=True,
+                                                            output_mode='int',
+                                                            name='Vectorizer')
+    vectorization_layer.adapt(data=df['sentence'].values)
     label_count = df['label'].nunique()
 
-    inputs=tf.keras.layers.Input(shape=(1,), dtype=tf.string)
-    vectors=vect_layer(inputs)
-    embeddings = tf.keras.layers.Embedding(
-        input_dim=len(vect_layer.get_vocabulary()), input_length=mp['output_sequence_length'],
-        output_dim=mp['hidden_size'], mask_zero=True, name='Embedder')(vectors)
-    bilstm = tf.keras.layers.Bidirectional(
-        layer=tf.keras.layers.LSTM(units=mp['hidden_size']), name='BiLSTM')(embeddings)
-    dense_1 = tf.keras.layers.Dense(units=mp['hidden_size'], activation='relu', name='Dense_1')(bilstm)
-    output_layer=tf.keras.layers.Dense(units=label_count, use_bias=True, activation='softmax', name='Output')(dense_1)
 
-    model = tf.keras.models.Model(inputs=inputs, outputs=output_layer)
+    model=define_model_BiLSTM(vectorization_layer,mp,label_count)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(1e-4),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(),
